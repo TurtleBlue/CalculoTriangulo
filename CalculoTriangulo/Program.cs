bf14b074e5ecb58e7438c45873a2da125f754015
Vinicius Nakano <vinit@TURTLEBLUE-PC>
2024-03-25 13:20:45 -0300

    Add project files.

---
+++ CalculoTriangulo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CalculoTriangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();    
+
+            Console.WriteLine("Insira as medidas do triângulo X:");
+
+            x.A = double.Parse(Console.ReadLine());
+            x.B = double.Parse(Console.ReadLine());
+            x.C = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n\nAgora insira as medidas do triângulo Y:");
+
+            y.A = double.Parse(Console.ReadLine());
+            y.B = double.Parse(Console.ReadLine());
+            y.C = double.Parse(Console.ReadLine());
+
+            double areaX = x.Area();
+            double areaY = y.Area();
+
+            Console.Clear();
+            Console.WriteLine($"A área do triangulo X é {areaX:n4} e a área do triangulo Y é {areaY:n4}");
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("A área do triangulo X é a maior");
+            } 
+            else
+            {
+                Console.WriteLine("A área do triangulo Y é a maior");
+            }
+        }
+    }
+}
